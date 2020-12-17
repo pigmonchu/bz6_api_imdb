a823f0e3c0b84eeb18b34e087eaf7138ea1a6d50
Ramón Maldonado Rodríguez <monterdi@gmail.com>
2020-12-18 00:31:26 +0100

    Refactorización de consulta api omdb

--- main.py
+++ main.py
@@ -1,36 +1,36 @@
 import requests
 
-pregunta = input("Titulo de la película")
-
 API_KEY="da22215a"
+#TODO - Arreglar urls para evitar tanta repeticion
+url_template = "http://www.omdbapi.com/?apikey={}&{}={}"
 
-direccion_elisabet = "http://www.omdbapi.com/?apikey="+API_KEY+"&s="+pregunta
-direccion_bito = f"http://www.omdbapi.com/?apikey={API_KEY}&s={pregunta}"
-direccion_josep = "http://www.omdbapi.com/?apikey={}&s={}".format(API_KEY, pregunta)
-
-respuesta = requests.get(direccion_josep)
-
-if respuesta.status_code == 200:
-    datos = respuesta.json()
-    if datos['Response'] == "False":
-        print(datos["Error"])
-    else:
-        primera_peli = datos['Search'][0]
-        clave = primera_peli['imdbID']
-
-        otra_direccion = "http://www.omdbapi.com/?apikey={}&i={}".format(API_KEY, clave)
-        nueva_respuesta = requests.get(otra_direccion)
-        if nueva_respuesta.status_code == 200:
-            datos = nueva_respuesta.json()
-            if datos['Response'] == "False":
-                print(datos["Error"])
-            else: 
-                titulo = datos['Title']
-                agno = datos['Year']
-                director = datos ['Director']
-                print("La peli {}, estrenada en el año {}, fue dirigida por {}".format(titulo, agno, director))
+def peticion(url):
+    respuesta = requests.get(url)
+    if respuesta.status_code == 200:
+        datos = respuesta.json()
+        if datos['Response'] == "False":
+            return datos["Error"]
         else: 
-            print("Error en consulta por id:", nueva_respuesta.status_code)
-else: 
-    print("Error en busqueda:", respuesta.status_code)
+            return datos
+    else: 
+        return "Error en consulta por id:", respuesta.status_code
+
+pregunta = input("Titulo de la película: ")
+
+respuesta = peticion(url_template.format(API_KEY, 's', pregunta))
+if isinstance(respuesta, str):
+    print(respuesta)
+else:
+    primera_peli = respuesta['Search'][0]
+    clave = primera_peli['imdbID']
+
+    respuesta = peticion(url_template.format(API_KEY, 'i', clave))
+    if isinstance(respuesta, str):
+        print(respuesta)
+    else:
+        titulo = respuesta['Title']
+        agno = respuesta['Year']
+        director = respuesta ['Director']
+        print("La peli {}, estrenada en el año {}, fue dirigida por {}".format(titulo, agno, director))
+
 
