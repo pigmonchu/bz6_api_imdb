1f5e926e1c6c630e1e4f80f9a23acab71d0d1b25
Ramón Maldonado Rodríguez <monterdi@gmail.com>
2020-12-21 21:22:43 +0100

    Bucle repite peli

--- main.py
+++ main.py
@@ -13,24 +13,27 @@ def peticion(url):
         else: 
             return datos
     else: 
-        return "Error en consulta por id:", respuesta.status_code
+        return "Error en consulta por id:" + respuesta.status_code
 
-pregunta = input("Titulo de la película: ")
+repetir = 'S'
+while repetir == 'S':
+    pregunta = input("Titulo de la película: ")
 
-respuesta = peticion(url_template.format(API_KEY, 's', pregunta))
-if isinstance(respuesta, str):
-    print(respuesta)
-else:
-    primera_peli = respuesta['Search'][0]
-    clave = primera_peli['imdbID']
-
-    respuesta = peticion(url_template.format(API_KEY, 'i', clave))
+    respuesta = peticion(url_template.format(API_KEY, 's', pregunta))
     if isinstance(respuesta, str):
         print(respuesta)
     else:
-        titulo = respuesta['Title']
-        agno = respuesta['Year']
-        director = respuesta ['Director']
-        print("La peli {}, estrenada en el año {}, fue dirigida por {}".format(titulo, agno, director))
+        primera_peli = respuesta['Search'][0]
+        clave = primera_peli['imdbID']
 
+        respuesta = peticion(url_template.format(API_KEY, 'i', clave))
+        if isinstance(respuesta, str):
+            print(respuesta)
+        else:
+            titulo = respuesta['Title']
+            agno = respuesta['Year']
+            director = respuesta ['Director']
+            print("La peli {}, estrenada en el año {}, fue dirigida por {}".format(titulo, agno, director))
 
+    repetir = input('Quieres otra peli? (S/N) ')
+    repetir = repetir.upper()
--- main_con_excepciones.py
+++ main_con_excepciones.py
@@ -1,6 +1,6 @@
 import requests
 
-API_KEY="da22215a"
+API_KEY="da22215 a"
 #TODO - Arreglar urls para evitar tanta repeticion
 url_template = "http://www.omdbapi.com/?apikey={}&{}={}"
 
@@ -16,22 +16,26 @@ def peticion(url):
         else: 
             return datos
     else: 
-        raise PeticionError("Error en consulta por id: {}".format(respuesta.status_code))
+        raise PeticionError("Error en consulta: {}".format(respuesta.status_code))
 
-pregunta = input("Titulo de la película: ")
+repetir = 'S'
+while repetir == 'S' or repetir == 's':
+    try:
+        pregunta = input("Titulo de la película: ")
+        respuesta = peticion(url_template.format(API_KEY, 's', pregunta))
+        primera_peli = respuesta['Search'][0]
+        clave = primera_peli['imdbID']
 
-try:
-    respuesta = peticion(url_template.format(API_KEY, 's', pregunta))
-    primera_peli = respuesta['Search'][0]
-    clave = primera_peli['imdbID']
+        respuesta = peticion(url_template.format(API_KEY, 'i', clave))
+        titulo = respuesta['Title']
+        agno = respuesta['Year']
+        director = respuesta ['Director']
+        print("La peli '{}', estrenada en el año {}, fue dirigida por {}".format(titulo, agno, director))
 
-    respuesta = peticion(url_template.format(API_KEY, 'i', clave))
-    titulo = respuesta['Title']
-    agno = respuesta['Year']
-    director = respuesta ['Director']
-    print("La peli '{}', estrenada en el año {}, fue dirigida por {}".format(titulo, agno, director))
-except PeticionError as e:
-    print(e)
+        repetir = input('Otra peli (S/N)? ')
+
+    except PeticionError as e:
+        print(e)
 
 
 
